2dc3f2d23d20dae8449141de1af51affa03dcbbe
Bryan Campbell <bryan@campbellsgambit.io>
2026-01-19 18:49:21 -0500

    boilerplate the cs-wordpress-blocks plugin

---
+++ wp-content/plugins/cs-wordpress-blocks/cs-wordpress-blocks.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Cs Wordpress Blocks
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     cs-wordpress-blocks
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Cs_Wordpress_Blocks
+ */
+
+// Your code starts here.
